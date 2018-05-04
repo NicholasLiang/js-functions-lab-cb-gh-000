64c268cad2276c7d0e190cee6b4cc6ba196c0f28
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 16:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var happyHolidays = () => "Happy holidays!"
 function happyHolidaysTo(name) {
-  
+  return 'Happy holidays, ' + name + '!'
 }
 var happyHolidaysTo = (name) => 'Happy holidays, '+ name +'!'
 var happyHolidaysTo = (holiday, name) => 'Happy ${holiday}, ${name}!'
