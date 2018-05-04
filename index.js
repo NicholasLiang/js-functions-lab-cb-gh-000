a09af088b036a0e067f17c9a59b89028ba4f06d1
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 16:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var happyHolidays = () => "Happy holidays!"
 function happyHolidaysTo(name) {
   return 'Happy holidays, ' + name + '!';
 }
-function happyHolidaysTo(holiday, name) {
-  return 'Happy ' + holiday + ', ' + name + '!';
-}
-var holidayCountdown = (holiday, days) => 'It\'s ${days} days until ${holiday}!'
+// function happyHolidaysTo(holiday, name) {
+//   return 'Happy ' + holiday + ', ' + name + '!';
+// }
+// var holidayCountdown = (holiday, days) => 'It\'s ${days} days until ${holiday}!'
